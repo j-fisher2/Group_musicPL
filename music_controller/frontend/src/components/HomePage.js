0342c9d618c0c9308547612666cfb14a5f6a7f65
Justin <justinfisher340@gmail.com>
2024-04-29 18:33:41 -0400

    config docs

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
+import Room from "./Room";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Link,
   Redirect,
+  useSearchParams,
 } from "react-router-dom";
 
 export default class HomePage extends Component {
   constructor(props) {
     super(props);
   }
-  
+
   render() {
     return (
       <Router>
@@ -21,6 +23,7 @@ export default class HomePage extends Component {
           <Route path="/" element={<p>Home Page</p>} />
           <Route path="/join" element={<RoomJoinPage />} />
           <Route path="/create" element={<CreateRoomPage />} />
+          <Route path="/room/:roomCode" element={<Room />} />
         </Routes>
       </Router>
     );
