10f86e1a76daf120731e476e08f22555771e1cca
Justin <justinfisher340@gmail.com>
2024-05-02 10:51:30 -0400

    view for room updates

---
+++ music_controller/frontend/src/components/UpdateRoomPage.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+
+export default function CreateRoomPage() {
+  const defaultVotes = 2;
+  const [guestCanPause, setGuestCanPause] = useState(true);
+  const [votesToSkip, setVotesToSkip] = useState(defaultVotes);
+  const navigate = useNavigate();
+
+  const handleVotesChange = (e) => {
+    setVotesToSkip(e.target.value);
+  };
+
+  const handleGuestCanPauseChange = (e) => {
+    setGuestCanPause(e.target.value === "true" ? true : false);
+  };
+
+  const handleRoomButtonPress = () => {
+    const requestOptions = {
+      method: "PATCH",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        votes_to_skip: votesToSkip,
+        guest_can_pause: guestCanPause,
+        code: localStorage.getItem("code"),
+      }),
+    };
+    fetch("/api/update-room", requestOptions)
+      .then((res) => res.json())
+      .then((data) => {
+        
+        alert("Room updated successfully");
+        });
+    };
+
+
+  return (
+    <Grid container spacing={1}>
+      <Grid item xs={12} align="center">
+        <Typography component="h4" variant="h4">
+          Update Room
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <FormControl component="fieldset">
+          <FormHelperText
+            align="center">Guest Control of Playback State
+          </FormHelperText>
+          <RadioGroup
+            row
+            defaultValue="true"
+            onChange={handleGuestCanPauseChange}
+          >
+            <FormControlLabel
+              value="true"
+              control={<Radio color="primary" />}
+              label="Play/Pause"
+              labelPlacement="bottom"
+            />
+            <FormControlLabel
+              value="false"
+              control={<Radio color="secondary" />}
+              label="No Control"
+              labelPlacement="bottom"
+            />
+          </RadioGroup>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <FormControl>
+          <TextField
+            onChange={handleVotesChange}
+            required={true}
+            type="number"
+            defaultValue={defaultVotes}
+            inputProps={{ min: 1, style: { textAlign: "center" } }}
+          ></TextField>
+          <FormHelperText
+            align="center">Votes Required to Skip Song
+          </FormHelperText>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={handleRoomButtonPress}
+        >
+          Update Room
+        </Button>
+      </Grid>
+    </Grid>
+  );
+}
